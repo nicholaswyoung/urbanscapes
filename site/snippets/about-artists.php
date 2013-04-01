34cad75809917e545e5180abbcc8131fe20e0268
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-01 15:05:00 -0500

    More tweaks.

--- site/snippets/about-artists.php
+++ site/snippets/about-artists.php
@@ -1,8 +1,8 @@
 <?php $artists = $pages->findByUID('about')->children() ?>
 <?php foreach ($artists as $artist): ?>
 	<div class='bio'>
-		<h2 class='name'><?php echo $artist->title() ?></h2>
 		<a class='profile-image <?php echo $artist->uid() ?>' href='<?php echo $artist->link() ?>'>&nbsp;</a>
+		<h2 class='name'><?php echo $artist->title() ?></h2>
 		<?php echo kirbytext($artist->text()) ?>
 		<p class='artist-link'>
 			<a href='<?php echo $artist->link() ?>'>Learn more about <?php echo $artist->title() ?> &raquo;</a>
