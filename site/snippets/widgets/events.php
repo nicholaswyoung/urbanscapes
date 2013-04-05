c6f83e289179e8376e001bd01afc4cc850db9506
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-05 10:07:31 -0500

    add events.

--- site/snippets/widgets/events.php
+++ site/snippets/widgets/events.php
@@ -1,5 +1,12 @@
 <div id='events' class='widget'>
 	<h2 class='subhead'>Upcoming Events</h2>
 	<ul class='basic-list'>
+		<?php $events = $pages->findByUID('events')->children()->visible() ?>
+		<?php foreach ($events as $event): ?>
+			<li class='event'>
+				<a href='<?php $event->link() ?>'><?php echo $event->date('M d') ?> &mdash; <?php echo $event->city() ?>, <?php echo $event->state() ?></a>
+				<?php echo kirbytext($event->text()) ?>
+			</li>
+		<?php endforeach ?>
 	</ul>
 </div>
