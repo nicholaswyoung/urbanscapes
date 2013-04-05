e263f01a350674b4ef707b6c268b6528ec2ade8f
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-05 13:11:54 -0500

    add message about prints purchasing.

--- site/snippets/widgets/newsletter.php
+++ site/snippets/widgets/newsletter.php
@@ -1,7 +1,7 @@
 <div class='widget newsletter'>
 	<h2 class='subhead'>Email Newsletter</h2>
 	<p>
-		Sign up for the email newsletter to learn where we'll be exhibiting!
+		Sign up for the email newsletter to learn where we'll be exhibiting, or to learn about purchasing prints!
 	</p>
 	<form class='madmimi' action="https://madmimi.com/signups/subscribe/75668" method="post" id="mad_mimi_signup_form" target="_blank"> 
 		<input id="signup_email" name="signup[email]" type="email" placeholder="you@example.com" />
