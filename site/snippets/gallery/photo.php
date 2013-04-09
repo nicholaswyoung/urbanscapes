f4a4ea6e957c0dd79ff8552e5ad2678335b6fac9
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-09 14:45:01 -0500

    Cleanup gallery styles.

--- site/snippets/gallery/photo.php
+++ site/snippets/gallery/photo.php
@@ -1,3 +1,6 @@
 <div class='photo <?php echo $photo->name() ?>'>
 	<img class='preview' src='<?php echo $photo->url() ?>' />
+	<div class='meta'>
+		<h3><?php echo $photo->title() ?></h3>
+	</div>
 </div>
