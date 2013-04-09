eba78d86875550ce8dff93e16ddeb9b490523ac7
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-09 13:50:52 -0500

    add gallery header image.

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -1,6 +1,7 @@
 <?php snippet('header') ?>
 <section class='content gallery'>
 	<?php foreach($page->images() as $photo): ?>
+		<?php if ($photo->name() == 'header') continue ?>
 		<?php snippet('gallery/photo', array('photo' => $photo)) ?>
 	<?php endforeach ?>
 </section>
