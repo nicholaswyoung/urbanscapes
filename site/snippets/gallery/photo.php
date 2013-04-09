9ceb33a4f35145bb4c2e05ca1da1da0cccfb948c
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-09 15:09:29 -0500

    More gallery stuff.

--- site/snippets/gallery/photo.php
+++ site/snippets/gallery/photo.php
@@ -2,5 +2,8 @@
 	<img class='preview' src='<?php echo $photo->url() ?>' />
 	<div class='meta'>
 		<h3><?php echo $photo->title() ?></h3>
+		<button class='add' data-id='<?php echo $photo->id() ?>'>
+			Add to Cart
+		</button>
 	</div>
 </div>
