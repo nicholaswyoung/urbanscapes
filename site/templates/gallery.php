60d2c0ec99f414991fc93b951f835093dbb9fc50
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-15 15:31:04 -0500

    update gallery with all Shoplocket panels.

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -4,13 +4,13 @@
 		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/69Fpb/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
 	<div class='photo dig'>
-		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/69Fpb/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/Z30mF/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
 	<div class='photo irradiated'>
-		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/69Fpb/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/vdE6y/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
 	<div class='photo spokes'>
-		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/69Fpb/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/cr6xo/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
 </section>
 <script src='https://www.shoplocket.com/assets/widgets/embed.js' type='text/javascript'></script>
