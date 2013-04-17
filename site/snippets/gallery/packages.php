71fade54a877fe6c56c112bfbfd73674ff4fdb25
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-17 15:01:19 -0500

    add store

---
+++ site/snippets/gallery/packages.php
@@ -0,0 +1,11 @@
+<div class='packages'>
+	<div class='photo collection-one barred dig'>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/EzdBS/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+	</div>
+	<div class='photo collection-two irradiated spokes'>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/lGjtC/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+	</div>
+	<div class='photo collection-all'>
+		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/waSBA/embed?style=modal' width='330' height='500' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
+	</div>
+</div>
