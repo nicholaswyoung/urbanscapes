c88aba65564d1d2820db4f571ac72edb3f92f0a1
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-17 15:04:51 -0500

    tweak

--- site/snippets/gallery/packages.php
+++ site/snippets/gallery/packages.php
@@ -1,11 +1,11 @@
 <div class='packages'>
-	<div class='photo collection-one barred dig'>
+	<div class='photo collection-one'>
 		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/EzdBS/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
-	<div class='photo collection-two irradiated spokes'>
+	<div class='photo collection-two'>
 		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/lGjtC/embed?style=modal' width='355' height='325' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
-	<div class='photo collection-all'>
+	<div class='photo series-one'>
 		<iframe class='shoplocket-embed' src='https://www.shoplocket.com/products/waSBA/embed?style=modal' width='330' height='500' frameborder='0' style='max-width:100%;' scrolling='no'></iframe>
 	</div>
 </div>
